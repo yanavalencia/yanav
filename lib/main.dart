a0804024e47c24dfeffb42c3cf79787aadcf8081
BatStateU <yanavalencia19@gmail.com>
2024-06-20 13:44:36 +0800

    Activity 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-appBar: AppBar(
-  title:Center(
-    child: Text('CHAWAPP'),
-  ),
-     backgroundColor: Colors.pink,
-),
-      body: Center(
-        child:Image.network('https://theskinnypot.com/wp-content/uploads/2023/03/Chow-King-Fried-Rice.jpg')
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.pink,
-        child: Icon(Icons.add),
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    backgroundColor: Colors.blueGrey,
+    appBar: AppBar(
+      title: Text ('User Profile'),
+      backgroundColor: Colors.pinkAccent,
+      centerTitle: true,
+    ),
+    body: Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'NAME',
+            style:TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 1.0,
+            ) ,
+          ),
+          Text(
+            'Yana Laurice A. Valencia',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'YEAR',
+            style:TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 1.0,
+            ) ,
+          ),
+          Text(
+            '3rd Year',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'EMAIL',
+            style:TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 1.0,
+            ) ,
+          ),
+          Text(
+            'yanalaurice@gmail.com',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+          ),
+        ],
       ),
     ),
-  ));
-}
+  ),
+));
